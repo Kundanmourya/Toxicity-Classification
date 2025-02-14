fb2f72ce92ca6b8d081728e11d8e7c542b504939
Kundansingh Mourya <85029026+Kundanmourya@users.noreply.github.com>
2025-02-14 08:05:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ def toxicity_prediction(text):
     class_name = "Toxic" if prediction == 1 else "Non-Toxic"
     return class_name
     
-st.title("Health Check Page")
-st.write("App is running smoothly!"
+#st.title("Health Check Page")
+#st.write("App is running smoothly!"
              
 # Set up the page title and layout
 st.set_page_config(page_title="Toxicity Detection App", layout="centered")
